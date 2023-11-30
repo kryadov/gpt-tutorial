347007dd88ea6fed5b6585413f29a4a3e2a10f53
Konstantin Ryadov <kryadov@azul.com>
2023-11-30 12:12:56 +0400

    bugfix + added comments

--- python/2.bard/1.one-simple-question/one-simple-question.py
+++ python/2.bard/1.one-simple-question/one-simple-question.py
@@ -4,5 +4,5 @@ from bardapi import Bard
 
 # also read https://github.com/dsdanielpark/Bard-API/issues/155
 # model = "LaMDA" # https://en.wikipedia.org/wiki/LaMDA
-bard_output = Bard(token='os.environ.get("_BARD_API_KEY")').get_answer("Is there life on Mars?")['content']
+bard_output = Bard(token=os.environ.get("_BARD_API_KEY")).get_answer("Is there life on Mars?")['content']
 print(bard_output)
